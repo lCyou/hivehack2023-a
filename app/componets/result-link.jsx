aadee844d912506b6527f7fb88a7227440a5569a
coder-izuki <118116104+coder-izuki@users.noreply.github.com>
2023-11-25 16:24:05 +0900

    結果ページへのリンクの設置

--- app/componets/result-link.jsx
+++ app/componets/result-link.jsx
@@ -1,6 +1,7 @@
 "use client"
 import Image from 'next/image'
 import styles from './result-link.module.css'
+import Link from 'next/link';
 // const votees = [{ name: '田中' }, { name: '田中' }, { name: '田中' }, { name: '田中' }, { name: '田中' }];
 // const eventName = "作品の人気投票";
 //const eventName = 
@@ -9,10 +10,15 @@ import styles from './result-link.module.css'
 export default function ResultLink() {
     const vote = () => {
         alert("田中");
+        //vote();
     }
     return (
         <>
-            <a>結果はこちら</a>
+            <br></br>
+            <br></br>
+            <Link className={styles['link']} href="../result">
+                結果はこちら
+            </Link>
         </>
     )
 }
