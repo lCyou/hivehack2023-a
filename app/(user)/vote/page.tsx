467ec2a259409e064eb19f9c1fbb5d66f5b21990
K.Yamakawa <87867174+lCyou@users.noreply.github.com>
2023-11-27 16:04:02 +0900

    ユーザーのためのルーティングを設定

---
+++ app/(user)/vote/page.tsx
@@ -0,0 +1,25 @@
+"use client"
+// import Image from 'next/image'
+import Link from 'next/link'
+import styles from './page.module.css'
+// import ResultLink from '../components/result-link'
+import { VoteButton } from '../../components/vote.jsx'
+
+// const votees = [{ name: '田中' }, { name: '田中' }, { name: '田中' }, { name: '田中' }, { name: '田中' }];
+const eventName = "作品の人気投票";
+
+export default function Home() {
+
+    const vote = () => {
+        alert("田中");
+    }
+    
+    return (
+        <div>
+            <h1 className={styles['title']}>{eventName}</h1>
+            <p className={styles['sentence']}>以下の中から投票したい人を選んでください！</p>
+            <VoteButton />
+            <Link href="/result">結果はこちら！</Link>
+        </div>
+    )
+}
